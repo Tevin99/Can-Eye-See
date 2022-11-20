b1182066621720b6a82bbc58c448679e3cdf94bd
hannahlizc <hannahcho1023@gmail.com>
2022-11-20 03:47:58 -0500

    completed

---
+++ stylesheets/take_pic.js
@@ -0,0 +1,13 @@
+(() => {
+  const width = 320;    // We will scale the photo width to this
+  const height = 0;     // This will be computed based on the input stream
+
+  const streaming = false;
+
+  let video = null;
+  let canvas = null;
+  let photo = null;
+  let startbutton = null;
+
+  console.log("hello world");
+})();
